f469fca2c5d784588d12a5580823fd0525978f67
Matteo Tiberti <matteo.tiberti@gmail.com>
2024-04-16 11:45:00 +0200

    modified template script so that it generates empty output in case of this failure mode

--- mavisp_templates/GENE_NAME/ptm/mutatex/run_ddgs.sh
+++ mavisp_templates/GENE_NAME/ptm/mutatex/run_ddgs.sh
@@ -5,7 +5,21 @@ list=$2
 
 #label file from PDB
 pdb2labels -p $pdb
-ddg2summary -p $pdb -d final_averages -l mutation_list.txt -L $list
-mv summary.txt summary_stability.txt
 
+set +e
+ddg2summary -p $pdb -d final_averages -l mutation_list.txt -L $list &> log
+exit_code=$?
+echo $exit_code
+
+if [[ $exit_code -eq 1 ]]; then
+    line=$(grep 'no usable mutations were found in mutations file; exiting...' log)
+    if [[ ! -z $line ]]; then
+        touch summary.txt
+    else
+        exit 1
+    fi
+fi
+set -e
+rm log
+mv summary.txt summary_stability.txt
 
